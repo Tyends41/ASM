0d49637d35b66a62a0114b42f238ac4a96200716
Tyends41 <141699209+Tyends41@users.noreply.github.com>
2024-04-17 16:28:49 +0700

    Add files via upload

---
+++ lithubbook/pages/carts/all.php
@@ -0,0 +1,19 @@
+<?php
+session_start(); // start a session
+
+if (!isset($_SESSION['user_id'])) {
+  header('Location: ../user/login.html'); // redirect to login page if $email is not in the session
+  exit;
+}
+
+// Connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'lithubbook');
+
+// Check connection
+if (!$db) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Select all books
+$result = mysqli_query($db, "SELECT * FROM carts");
+?>
